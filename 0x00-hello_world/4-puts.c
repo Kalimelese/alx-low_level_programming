62302db362a8c8532534aeff9f47f76da6da94a9
kalkidan melese <kalimelese3@gmail.com>
2022-06-30 13:40:02 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
+#include <string.h>
 
-/
-
+/**
  * main - Entry point
- *
- *  * Return: Always 0 (sucess)
- *
+ * Description: prints "\"Programming is like building a multilingual puzzle"*
+ * Return: Always 0 (sucess)
 */
 int main(void)
 
